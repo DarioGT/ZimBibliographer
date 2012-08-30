7e0d08896bb52254127b489893a88f0796b4e61f
François Boulogne <fboulogne@sciunto.org>
2012-08-30 16:12:35 +0200

    edit the text

--- ZimBibliographer/processtext.py
+++ ZimBibliographer/processtext.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import re
-
+import os.path
 from ZimBibliographer.bibtexparser import BibTexParser
 
 def process_text(original_text, bibtex):
@@ -20,6 +20,13 @@ def process_text(original_text, bibtex):
     modified text
     """
 
+    #In case of a relative path
+    bibtex = os.path.expanduser(bibtex)
+    basepath = os.path.dirname(bibtex)
+
+    ###########
+    # Bibtex
+    ###########
     with open(bibtex, 'r') as bibfile:
         bibliography = BibTexParser(bibfile)
 
@@ -34,10 +41,24 @@ def process_text(original_text, bibtex):
 
     keys = citecommand.findall(copy_text)
 
+    ###########
+    # Edit text
+    ###########
     for key in keys:
         print(key)
-        print(entries_hash[key]['file'])
+        path = entries_hash[key]['file']
+
+        #Jabref codes path like ":/tmp/file.pdf:PDF"
+        # or ":file.pdf:PDF"
+        #For the second case, jabref write comments with metadata
+        path = re.sub(':(.*):[a-zA-Z]+', "\\1", path)
+        #TODO deal with this second case... :(
+
+
         #TODO
         #Modify the text. Use foo for bibtex data
+        cite = 'cite{' + key + '}'
+        internal_link = '[[' + str(path) + ']]' #FIXME
+        copy_text = re.sub(cite, internal_link, copy_text)
 
     return copy_text
